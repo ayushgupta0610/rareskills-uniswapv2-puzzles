96761025cc8e75ce64bf7390d76bcbb844c0a540
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-07-24 13:33:13 +0530

    Add solution for ExactSwapWithRouter

--- src/ExactSwapWithRouter.sol
+++ src/ExactSwapWithRouter.sol
@@ -24,9 +24,31 @@ contract ExactSwapWithRouter {
         address[] memory path = new address[](2);
         path[0] = weth;
         path[1] = usdc;
-        uint256 wethAmountIn = 1 ether; // This needs to be calculated such that we get exactly 1337 USDC out
-        uint256 usdcAmountOutMin = 1337 * 1e6;
-        _router.swapExactTokensForTokens(wethAmountIn, usdcAmountOutMin, path, address(this), deadline);
+        uint256 amountOut = 1337 * 1e6;
+        address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f; // TODO: Check with Jeffrey if this is allowed (external info)
+        address pair = pairFor(factory, usdc, weth);
+        uint256 reserveIn = IERC20(weth).balanceOf(pair);
+        uint256 reserveOut = IERC20(usdc).balanceOf(pair);
+        uint256 wethAmountIn = getAmountIn(amountOut, reserveIn, reserveOut);
+        _router.swapExactTokensForTokens(wethAmountIn, amountOut, path, address(this), deadline);
+    }
+
+    // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
+        require(amountOut > 0, 'INSUFFICIENT_OUTPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'INSUFFICIENT_LIQUIDITY');
+        uint numerator = reserveIn *amountOut*1000;
+        uint denominator = (reserveOut-amountOut) * 997;
+        amountIn = numerator / denominator + 1;
+    }
+
+     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        pair = address(uint160(uint256((keccak256(abi.encodePacked(
+                hex'ff',
+                factory,
+                keccak256(abi.encodePacked(tokenA, tokenB)),
+                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+            ))))));
     }
 }
 
